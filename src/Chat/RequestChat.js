376c9c623143863a68a03ab2afc192c3fb9e0931
park ye jin <dyddyd0514@naver.com>
2021-08-11 01:55:19 +0900

    Feat: Chatting header component

--- src/Chat/RequestChat.js
+++ src/Chat/RequestChat.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ChattingHeader from '../components/ChattingHeader';
 
-const RequestChat = props => {return}
+const RequestChat = (props) => {
+  return (
+    <>
+      <ChattingHeader />
+    </>
+  );
+};
 
 export default RequestChat;
